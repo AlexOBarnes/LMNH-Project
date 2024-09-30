9410a0a02f290eb9f6a79bf7dbb1b63fc61d662b
AlexOBarnes <a.o.barnes222@gmail.com>
2024-09-30 18:27:00 +0100

    Added constraints

--- database/schema.sql
+++ database/schema.sql
@@ -8,27 +8,27 @@ DROP TABLE IF EXISTS botanists;
 DROP TABLE IF EXISTS plants;
 
 CREATE TABLE countries (
-    country_id SMALLINT NOT NULL,
-    country_code VARCHAR(8) NOT NULL,
-    country_name VARCHAR(255) NOT NULL,
+    country_id SMALLINT NOT NULL AUTO_INCREMENT,
+    country_code VARCHAR(8) NOT NULL UNIQUE,
+    country_name VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY(country_id)
 );
 
 CREATE TABLE plant_species (
-    plant_species_id INT NOT NULL,
+    plant_species_id INT NOT NULL AUTO_INCREMENT,
     common_name VARCHAR(255) NOT NULL,
-    scientific_name VARCHAR(255) NOT NULL,
+    scientific_name VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY(plant_species_id)
 );
 
 CREATE TABLE continents (
-    continent_id SMALLINT NOT NULL,
-    continent_name VARCHAR(255) NOT NULL,
+    continent_id SMALLINT NOT NULL AUTO_INCREMENT,
+    continent_name VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY(continent_id)
 );
 
 CREATE TABLE regions (
-    town_id INT NOT NULL,
+    town_id INT NOT NULL AUTO_INCREMENT,
     town_name VARCHAR(255) NOT NULL,
     continent_id SMALLINT NOT NULL,
     country_id SMALLINT NOT NULL,
@@ -38,25 +38,25 @@ CREATE TABLE regions (
 );
 
 CREATE TABLE origins (
-    location_id INT NOT NULL,
-    longitude FLOAT(53) NOT NULL,
-    latitude FLOAT(53) NOT NULL,
+    location_id INT NOT NULL AUTO_INCREMENT,
+    longitude FLOAT(53) NOT NULL UNIQUE,
+    latitude FLOAT(53) NOT NULL UNIQUE,
     town_id INT NOT NULL,
     PRIMARY KEY(location_id),
     FOREIGN KEY(town_id) REFERENCES regions(town_id)
 );
 
 CREATE TABLE botanists (
-    botanist_id INT NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    botanist_id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY(botanist_id)
 );
 
 CREATE TABLE plants (
-    plant_id INT NOT NULL,
+    plant_id INT NOT NULL AUTO_INCREMENT,
     location_id INT NOT NULL,
     last_watering DATETIME NOT NULL,
     plant_species_id INT NOT NULL,
@@ -66,8 +66,8 @@ CREATE TABLE plants (
 );
 
 CREATE TABLE recordings (
-    recording_id BIGINT NOT NULL,
-    time DATETIME NOT NULL,
+    recording_id BIGINT NOT NULL AUTO_INCREMENT,
+    time_taken DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP CHECK (time_taken <= CURRENT_TIMESTAMP),
     soil_moisture FLOAT(53) NOT NULL,
     temperature FLOAT(53) NOT NULL,
     plant_id INT NOT NULL,
