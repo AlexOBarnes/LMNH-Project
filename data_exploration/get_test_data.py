001e33fc97bb33858c76b436420a16477627d9e4
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-01 09:43:14 +0100

    added changes to catch error with num plants misreported on front page

--- data_exploration/get_test_data.py
+++ data_exploration/get_test_data.py
@@ -1,10 +1,11 @@
 '''Running this script produces a csv file containing the LMNH plant data for available plants.'''
-
 import requests
 import pandas as pd
 import json
 import datetime
+
 BASE_URL = "https://data-eng-plants-api.herokuapp.com"
+MAX_NOT_FOUND_STREAK = 5
 
 
 def get_url(id: int):
@@ -16,8 +17,10 @@ def get_url(id: int):
 def get_num_plants() -> int:
     '''Return the number of plants on display.'''
     response = requests.get(BASE_URL, timeout=10).json()
+
     if response.get("success", False):
         return int(response["plants_on_display"])
+
     return 0
 
 
@@ -41,24 +44,35 @@ def get_results(response: dict):
 if __name__ == "__main__":
     data = []
     num_found = 0
+
     num_expected = get_num_plants()
+
     i = 0
+    not_found_streak = 0
     while True:
 
         if num_found == num_expected:
             break
 
         url = get_url(i)
+
         response = requests.get(url, timeout=10)
 
         if response.status_code == 200:
+            not_found_streak = 0
             num_found += 1
-            print(i)
             try:
                 data.append(get_results(response.json()))
 
             except Exception as err:
                 print(err)
+        else:
+            not_found_streak += 1
+            print(response.json())
+
+        if not_found_streak >= MAX_NOT_FOUND_STREAK:
+            print("Exceeded not found streak")
+            break
 
         i += 1
 
