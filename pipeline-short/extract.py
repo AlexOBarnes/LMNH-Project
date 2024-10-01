a07cb6f6eb0d303ff8fc0e91213da2145e58a405
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-01 15:08:01 +0100

    refactoring

--- pipeline-short/extract.py
+++ pipeline-short/extract.py
@@ -1,6 +1,8 @@
 '''Extraction of the data'''
 import logging
+
 import requests
+
 import asyncio
 import pandas as pd
 
@@ -11,6 +13,7 @@ BASE_URL = "https://data-eng-plants-api.herokuapp.com"
 
 def get_url(id: int):
     '''Gets the API url'''
+
     url = f"{BASE_URL}/plants/{id}"
     return url
 
@@ -21,12 +24,20 @@ def get_num_plants() -> int:
     response = requests.get(BASE_URL, timeout=10)
 
     if response.status_code != 200:
+        logging.error("Invalid response %s", response.status_code)
         raise ValueError("Request unsuccessful.")
 
-    if response.json().get("success", False):
-        return int(response["plants_on_display"])
+    num_plants = response.json().get("success", False)
+    if num_plants is not False:
+        logging.info("Number of plants on display is %s", num_plants)
+        return int(num_plants)
 
     return 0
+
+
+def validate_reponse(response: dict) -> bool:
+    '''Return True if a response is valid'''
+    if not response.get("id",)
 
 
 def extract() -> pd.DataFrame:
