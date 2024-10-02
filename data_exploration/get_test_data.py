5075f24169f0d6562db5f16d3f959311069bb5cc
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-02 17:07:22 +0100

    Formatted code

--- data_exploration/get_test_data.py
+++ data_exploration/get_test_data.py
@@ -1,58 +1,55 @@
 '''Running this script produces a csv file containing the LMNH plant data for available plants.'''
-
-import requests
-import pandas as pd
-import json
 import datetime
+import requests as req
+import pandas as pd
 BASE_URL = "https://data-eng-plants-api.herokuapp.com"
 
 
-def get_url(id: int):
+def get_url(plant_id: int):
     '''Gets the API url'''
-    url = f"{BASE_URL}/plants/{id}"
-    return url
+    return f"{BASE_URL}/plants/{plant_id}"
 
 
 def get_num_plants() -> int:
     '''Return the number of plants on display.'''
-    response = requests.get(BASE_URL, timeout=10).json()
-    if response.get("success", False):
-        return int(response["plants_on_display"])
+    res= req.get(BASE_URL, timeout=10).json()
+    if res.get("success", False):
+        return int(res["plants_on_display"])
     return 0
 
 
-def get_results(response: dict):
+def get_results(res: dict):
+    '''Returns the key information from the API response'''
     return {
-        "plant_id": response.get("plant_id"),
-        "botanist_email": response.get("botanist", {"email": None}).get("email"),
-        "botanist_name": response.get("botanist", {"name": None}).get("name"),
-        "botanist_phone": response.get("botanist", {"phone": None}).get("phone"),
-        "images_license_url": response.get("images", {"license_url": None}).get("license_url"),
-        "last_watered": response.get("last_watered"),
-        "name": response.get("name"),
-        "scientific_name": response.get("scientific_name"),
-        "origin_location": response.get("origin_location"),
-        "soil_moisture": response.get("soil_moisture"),
-        "temperature": response.get("temperature"),
-        "recording_taken": response.get("recording_taken")
+        "plant_id": res.get("plant_id"),
+        "botanist_email": res.get("botanist", {"email": None}).get("email"),
+        "botanist_name": res.get("botanist", {"name": None}).get("name"),
+        "botanist_phone": res.get("botanist", {"phone": None}).get("phone"),
+        "images_license_url": res.get("images", {"license_url": None}).get("license_url"),
+        "last_watered": res.get("last_watered"),
+        "name": res.get("name"),
+        "scientific_name": res.get("scientific_name"),
+        "origin_location": res.get("origin_location"),
+        "soil_moisture": res.get("soil_moisture"),
+        "temperature": res.get("temperature"),
+        "recording_taken": res.get("recording_taken")
     }
 
 
 if __name__ == "__main__":
     data = []
-    num_found = 0
-    num_expected = get_num_plants()
+    found = 0
+    expected = get_num_plants()
     i = 0
     while True:
 
-        if num_found == num_expected:
+        if found == expected:
             break
 
-        url = get_url(i)
-        response = requests.get(url, timeout=10)
+        response = req.get(get_url(i), timeout=10)
 
         if response.status_code == 200:
-            num_found += 1
+            found += 1
             print(i)
             try:
                 data.append(get_results(response.json()))
