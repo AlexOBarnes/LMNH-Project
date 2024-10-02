940edac601975114db5626825e8b977230c56b96
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-02 12:23:59 +0100

    back

--- pipeline-short/test_extract.py
+++ pipeline-short/test_extract.py
@@ -4,7 +4,7 @@
 from unittest.mock import patch
 import pytest
 
-from extract import get_url
+from extract import get_url, validate_response
 
 MOCK_BASE_URL = "https://api.example.com"
 
@@ -15,3 +15,22 @@ def test_get_url_with_valid_id():
     expected_url = f"{MOCK_BASE_URL}/plants/{id}"
 
     assert get_url(id) == expected_url
+
+
+def test_validate_response_invalid():
+    """Test for an invalid response."""
+    response = {
+        "field1": "value1",
+        "field2": "value2",
+        "field3": "value3",
+    }
+    assert validate_response(response) is False
+
+
+def test_validate_response_():
+    """Test for a response that's valid."""
+    response = {
+        "botanist": "value1",
+        "plant_id": "value3",
+    }
+    assert validate_response(response) is True
