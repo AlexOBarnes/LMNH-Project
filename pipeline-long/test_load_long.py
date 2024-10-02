9d392f5382bdbabcef6c36ccd6e5ed1102d5f654
India <india.howell123@gmail.com>
2024-10-02 16:09:06 +0100

    Basic test for upload_csv_to_s3 function.

--- pipeline-long/test_load_long.py
+++ pipeline-long/test_load_long.py
@@ -1,6 +1,7 @@
 """File to test the function in load.py"""
 
 from unittest.mock import patch, MagicMock
+from datetime import datetime
 from load import upload_to_csv
 
 
@@ -15,3 +16,14 @@ class TestUploadCSVtoS3:
         csv_content = "id,name,moisture\n1,Plant A,30.5\n2,Plant B,45.0\n"
         bucket_name = "test-bucket"
         mock_s3 = MagicMock()
+        mock_boto_client.return_value = mock_s3
+
+        upload_to_csv(csv_content, bucket_name)
+
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        expected_filename = f"plant_sensor_data_{current_date}.csv"
+        mock_s3.put_object.assert_called_once_with(
+            Bucket=bucket_name,
+            Key=expected_filename,
+            Body=csv_content
+        )
