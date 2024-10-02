f1cc8dc539f92390bf7e28b0b792bdc30adb6c3c
India <india.howell123@gmail.com>
2024-10-02 12:35:08 +0100

    Updated tests and function with proper error handling.

--- pipeline-long/test_transform_long.py
+++ pipeline-long/test_transform_long.py
@@ -10,14 +10,14 @@ class TestTransformDataToCSV:
             {"id": 2, "name": "Plant B", "moisture": 45.0}
         ]
 
-        expected_output = "id,name,moisture\r\n1,Plant A,30.5\r\n2,Plant B,45.0\r\n"
+        expected_output = "id,name,moisture\n1,Plant A,30.5\n2,Plant B,45.0\n"
         csv_file_like = transform_data_to_csv(data)
 
         assert csv_file_like.getvalue() == expected_output
 
     def test_empty_input(self):
         data = []
-        expected_output = "id,name,moisture\r\n"
-        csv_file_file = transform_data_to_csv(data)
+        expected_output = "id,name,moisture\n"
+        csv_file_like = transform_data_to_csv(data)
 
-        assert csv_file_file.getvalue() == expected_output
+        assert csv_file_like.getvalue() == expected_output
