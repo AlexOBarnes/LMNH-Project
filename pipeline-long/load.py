a15137b69744bf59c856c162a8a3d7584dfed162
India <india.howell123@gmail.com>
2024-10-02 17:16:11 +0100

    Adding if __name__ == __main__ block to load.py

--- pipeline-long/load.py
+++ pipeline-long/load.py
@@ -1,6 +1,7 @@
 """A file to load the CSV file into a specified S3 bucket."""
 
 from datetime import datetime
+import sys
 import boto3
 
 
@@ -12,3 +13,11 @@ def upload_csv_to_s3(csv_buffer: str, bucket_name: str):
 
     s3_client = boto3.client("s3")
     s3_client.put_object(Bucket=bucket_name, Key=filename, Body=csv_buffer)
+
+
+if __name__ == "__main__":
+
+    csv_buffer = sys.stdin.read()
+
+    bucket_name = "c13-wshao-lmnh-long-term-storage"
+    upload_csv_to_s3(csv_buffer, bucket_name)
