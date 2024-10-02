d38132f10bb1281dc124635034e6430e61c2d137
India <india.howell123@gmail.com>
2024-10-02 12:18:31 +0100

    Basic test for function in transform.py

---
+++ pipeline-long/test_transform_long.py
@@ -0,0 +1,16 @@
+from io import StringIO
+from transform import transform_data_to_csv
+
+
+class TestTransformDataToCSV:
+
+    def test_basic_functionality(self):
+        data = [
+            {"id": 1, "name": "Plant A", "moisture": 30.5},
+            {"id": 2, "name": "Plant B", "moisture": 45.0}
+        ]
+
+        expected_output = "id,name,moisture\r\n1,Plant A,30.5\r\n2,Plant B,45.0\r\n"
+        csv_file_like = transform_data_to_csv(data)
+
+        assert csv_file_like.getvalue() == expected_output
