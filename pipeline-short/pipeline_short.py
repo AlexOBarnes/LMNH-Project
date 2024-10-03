24f41d120d0a175f13422c05affe185e0eb89ad9
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-03 12:44:13 +0100

    created lambda function

--- pipeline-short/pipeline_short.py
+++ pipeline-short/pipeline_short.py
@@ -4,5 +4,23 @@ from extract_short import extract
 from transform_short import get_connection
 from load_short import upsert_plants
 
+
+def lambda_handler(event, context):
+    '''Lambda handler'''
+    try:
+        data = extract()
+    except Exception as err:
+        return {"statusCode": 400, 'body': f"Failure. Could not extract data, {err}"}
+    with get_connection() as conn:
+        try:
+            conn_cursor = conn.cursor()
+            upsert_plants(conn_cursor, data)
+            conn_cursor.commit()
+            conn_cursor.close()
+        except Exception as err:
+            return {"statusCode": 400, 'body': f"Failure: {err}"}
+    return {'statusCode': 200, "body": "Success."}
+
+
 if __name__ == "__main__":
     ...
