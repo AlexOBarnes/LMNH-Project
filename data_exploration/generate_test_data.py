c201d69169dd11e0e5db51a128c175363581879b
AlexOBarnes <a.o.barnes222@gmail.com>
2024-10-03 10:13:29 +0100

    Implemented RDS insertion function

--- data_exploration/generate_test_data.py
+++ data_exploration/generate_test_data.py
@@ -6,8 +6,18 @@ from io import StringIO
 import random
 from dotenv import load_dotenv
 import pandas as pd
+from pyodbc import connect
 from boto3 import client
 
+INVALID_KEYS = [7,10,13,15,18,23,24,27,37,40,43]
+
+def get_connection():
+    '''Returns a connection to the RDS database'''
+    return connect(f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+                   f"SERVER={ENV['HOST']},{ENV['DB_PORT']};"
+                   f"DATABASE={ENV['DB_NAME']};"
+                   f"UID={ENV['DB_USER']};"
+                   f"PWD={ENV['DB_PW']}")
 
 
 def parse_arguments() -> int:
@@ -15,20 +25,24 @@ def parse_arguments() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("--rows", "-r", type=int, default=50,
                         help="""Enter the number of rows you want the end csv file to be""")
+    parser.add_argument('--database','-d',action='store_true',
+                        help='Use this flag to input data into the database')
     args = parser.parse_args()
     rows = args.rows
+    database = args.database
 
 
-    return rows
+    return rows,database
 
 def create_plant_dataframe(rows:int) -> pd.DataFrame:
     '''Returns a pandas dataframe containing fake data'''
     data = []
+    valid_plant_ids = [i for i in range(50) if i not in INVALID_KEYS]
     for i in range(1, rows+1):
         timestamp = datetime.now()+timedelta(minutes=i)
-        moisture= round(random.uniform(0.0, 100.0), 3)
-        temperature = round(random.uniform(0.0, 40.0), 3)
-        plant = random.randint(1, 3)
+        moisture= round(random.uniform(20.0, 80.0), 3)
+        temperature = round(random.uniform(0.0, 30.0), 3)
+        plant = random.choice(valid_plant_ids)
         botanist = random.randint(1, 3)
         data.append([i, timestamp,
                     moisture, temperature, plant, botanist])
@@ -53,11 +67,24 @@ def upload_data(data: pd.DataFrame) -> None:
     aws_client.put_object(Bucket=ENV["BUCKET"],
                           Key=f'recordings/{date}.csv', Body=encoded_csv)
 
+def upload_data_db(data: pd.DataFrame) -> None:
+    '''Uploads data to the RDS database'''
+    q = '''INSERT INTO gamma.recordings 
+    (soil_moisture,temperature,plant_id,botanist_id)
+    VALUES (?,?,?,?)'''
+    plant_data = data.drop(columns=['recording_id','timestamp']).values.tolist()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.executemany(q,plant_data)
+            conn.commit()
 
 
 
 if __name__ == '__main__':
     load_dotenv()
-    iterations = parse_arguments()
+    iterations,database = parse_arguments()
     plant_df = create_plant_dataframe(iterations)
-    upload_data(plant_df)
+    if database:
+        upload_data_db(plant_df)
+    else:
+        upload_data(plant_df)
