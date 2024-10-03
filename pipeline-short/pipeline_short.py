b2fd31b391765ebe23e3fbe2e5e8c9cc2c4204ff
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-03 18:51:36 +0100

    add lambda handler

--- pipeline-short/pipeline_short.py
+++ pipeline-short/pipeline_short.py
@@ -1,24 +1,14 @@
 '''Short term pipeline'''
 
-from extract_short import extract
-from transform_short import get_connection
-from load_short import upsert_plants
+from load_short import load
 
 
 def lambda_handler(event, context):
     '''Lambda handler'''
     try:
-        data = extract()
+        load()
     except Exception as err:
         return {"statusCode": 400, 'body': f"Failure. Could not extract data, {err}"}
-    with get_connection() as conn:
-        try:
-            conn_cursor = conn.cursor()
-            upsert_plants(conn_cursor, data)
-            conn_cursor.commit()
-            conn_cursor.close()
-        except Exception as err:
-            return {"statusCode": 400, 'body': f"Failure: {err}"}
     return {'statusCode': 200, "body": "Success."}
 
 
