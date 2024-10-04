1e6306dae40c3ce111d0a04a109da372482c9b72
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-04 14:19:51 +0100

    added pylint skipfile

--- pipeline-short/test_database_functions.py
+++ pipeline-short/test_database_functions.py
@@ -1,10 +1,7 @@
-import pytest
 from unittest.mock import MagicMock
 from database_functions import (get_all_plant_ids, map_plant_id_to_most_recent_botanist,
                                 map_botanist_details_to_id, map_town_name_to_id,
-                                map_species_names_to_species_id, map_country_code_to_id,
-                                map_longitude_and_latitude_to_location_id,
-                                map_continent_name_to_id, get_max_location_id)
+                                map_species_names_to_species_id)
 
 
 def test_get_all_plant_ids():
